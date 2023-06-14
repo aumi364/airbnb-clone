ff606b5e7f9e27d8bdb4f9cab3810cbcdcd340b7
Asif Ahmed <aumi.asif@gmail.com>
2023-06-15 01:50:50 +0600

    feat - create logo component

---
+++ src/app/components/assets/logo.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+type Props = {}
+
+const Logo = (props: Props) => {
+    const router = useRouter
+    return (
+        <Image
+            className="hidden md:block cursor-pointer"
+            height="100"
+            width="100"
+            src={'/images/logo.png'}
+            alt={'Logo'}
+        />
+    )
+}
+
+export default Logo
